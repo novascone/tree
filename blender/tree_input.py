978679b0bd01baa73176031f3b23eb3e4b073561
Novascone <warren.novascone@icloud.com>
2026-06-08 15:03:52 -0600

    Increased performance, refined animation

--- blender/tree_input.py
+++ blender/tree_input.py
@@ -7,11 +7,12 @@ from .parser.convert import convert
 from .parser.translate import translate
 from .mesh import build_mesh
 from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty, FloatProperty, PointerProperty
+from bpy.props import StringProperty, FloatProperty, IntProperty
 from bpy.types import Operator
 import bpy
 import colorsys
 import math
+import random
 
 geometry = None
 read = None
@@ -59,9 +60,26 @@ def menu_func_import(self, context):
 
 class TREEProperties(bpy.types.PropertyGroup):
     interval_start: FloatProperty(name="Interval Start", default=0.0)
-    interval_end: FloatProperty(name="Interval End", default=3.0)
+    interval_end: FloatProperty(name="Interval End", default=10800)
     step_size: FloatProperty(name="Step Size", default=0.1)
+    anim_speed: FloatProperty(name="Anim Speed", default=0.01, min=0.001, max=1.0)
+    spot_width: FloatProperty(name="Spot Width", default=0.1, min=0.01, max=1.0) 
+    spot_strength: FloatProperty(name="Spot Strength", default=1.0)
+    seeds_per_level: IntProperty(name="Seeds Per Level", default=50, min=1)
+    alt_min: FloatProperty(name="Alt Min (km)", default=83.0)
+    alt_max: FloatProperty(name="Alt Max (km)", default=90.0)
+    alt_step: FloatProperty(name="Alt Step (km)", default=1.0, min=0.1)
 
+def fibonacci_sphere(n):
+    golden = (1 + math.sqrt(5)) / 2
+    points = []
+    for i in range(n):
+        theta = 2 * math.pi * i /golden
+        phi = math.acos(1 - 2 * (i + 0.5) / n)
+        lat = math.degrees(math.pi / 2 - phi)
+        lon = math.degrees(theta % (2 * math.pi)) - 180
+        points.append((lat, lon))
+    return points
 
 class ComputeStreamlines(Operator):
     """Get the path for the streamline"""
@@ -71,7 +89,14 @@ class ComputeStreamlines(Operator):
     def execute(self, context):
         global streamlines
         props = context.scene.tree_props
-        seeds = [[lat, lon, 86.0] for lat in range(0, 31, 6) for lon in range(0, 31, 6)]
+        fib = fibonacci_sphere(props.seeds_per_level)
+        alt = props.alt_min
+        seeds = []
+        while alt <= props.alt_max + 1e-6:
+            for lat, lon in fib:
+                seeds.append([lat, lon, alt])
+            alt += props.alt_step 
+        #seeds = [[lat, lon, 86.0] for lat in range(0, 31, 6) for lon in range(0, 31, 6)]
         streamlines = tree_core.driveField(read, seeds, props.interval_start, props.interval_end, props.step_size)
         return {'FINISHED'}
 
@@ -108,26 +133,49 @@ def mat_nodes(context):
     attr.attribute_name = 'arc_param'
     attr.attribute_type = 'GEOMETRY'
     spot_pos = nodes.new('ShaderNodeValue')
+
+    phase = nodes.new('ShaderNodeAttribute')
+    phase.attribute_name = 'phase'
+    phase.attribute_type = 'GEOMETRY'
+
+    add_phase = nodes.new('ShaderNodeMath')
+    add_phase.operation = 'ADD'
+
+    modulo = nodes.new('ShaderNodeMath')
+    modulo.operation = 'MODULO'
+    modulo.inputs[1].default_value = 1.0
     
     # distance = abs(factor - spot_pos)
     subtract = nodes.new('ShaderNodeMath')
     subtract.operation = 'SUBTRACT'
+
     absolute = nodes.new('ShaderNodeMath')
     absolute.operation = 'ABSOLUTE'
 
-    # brightness = max(0, 1 - distance / spot_width)
-    divide = nodes.new('ShaderNodeMath')
-    divide.operation = 'DIVIDE'
-    divide.inputs[1].default_value = 0.1 # spot_width
+    minimum = nodes.new('ShaderNodeMath')
+    minimum.operation = 'MINIMUM'
 
     subtract2 = nodes.new('ShaderNodeMath')
     subtract2.operation = 'SUBTRACT'
     subtract2.inputs[0].default_value = 1.0
 
+    # brightness = max(0, 1 - distance / spot_width)
+    divide = nodes.new('ShaderNodeMath')
+    divide.operation = 'DIVIDE'
+    divide.inputs[1].default_value = props.spot_width # spot_width
+
+    subtract3 = nodes.new('ShaderNodeMath')
+    subtract3.operation = 'SUBTRACT'
+    subtract3.inputs[0].default_value = 1.0
+
     maximum = nodes.new('ShaderNodeMath')
     maximum.operation = 'MAXIMUM'
-    maximum.inputs[1].default_value = 0.0 
+    maximum.inputs[1].default_value = 0.0
 
+    multiply = nodes.new('ShaderNodeMath')
+    multiply.operation = 'MULTIPLY'
+    multiply.inputs[1].default_value = props.spot_strength
+ 
     add = nodes.new('ShaderNodeMath')
     add.operation = 'ADD'
     add.inputs[1].default_value = 0.1
@@ -137,25 +185,32 @@ def mat_nodes(context):
     emission.inputs['Color'].default_value = (r, g, b, 0.1)
 
 
-    anim_speed = 0.01
+    anim_speed = props.anim_speed 
 
     driver = spot_pos.outputs[0].driver_add('default_value')
     driver.driver.expression = f"(frame * {anim_speed}) % 1.0"
  
-    links.new(attr.outputs['Fac'], subtract.inputs[0])
-    links.new(spot_pos.outputs['Value'], subtract.inputs[1])
+    links.new(attr.outputs['Fac'], subtract.inputs[0]) 
+    links.new(spot_pos.outputs['Value'], add_phase.inputs[0])
+    links.new(phase.outputs['Fac'], add_phase.inputs[1])
+    links.new(add_phase.outputs['Value'], modulo.inputs[0])
+    links.new(modulo.outputs['Value'], subtract.inputs[1])
     links.new(subtract.outputs['Value'], absolute.inputs[0])
-    links.new(absolute.outputs['Value'], divide.inputs[0])
-    links.new(divide.outputs['Value'], subtract2.inputs[1])
-    links.new(subtract2.outputs['Value'], maximum.inputs[0])
-    links.new(maximum.outputs['Value'], add.inputs[0])
+    links.new(absolute.outputs['Value'], minimum.inputs[0])
+    links.new(absolute.outputs['Value'], subtract2.inputs[1])
+    links.new(subtract2.outputs['Value'], minimum.inputs[1])
+    links.new(minimum.outputs['Value'], divide.inputs[0])
+    links.new(divide.outputs['Value'], subtract3.inputs[1])
+    links.new(subtract3.outputs['Value'], maximum.inputs[0])
+    links.new(maximum.outputs['Value'], multiply.inputs[0])
+    links.new(multiply.outputs['Value'], add.inputs[0])
     links.new(add.outputs['Value'], emission.inputs['Strength'])
     links.new(emission.outputs['Emission'], output.inputs['Surface'])
 
     mat.diffuse_color = (r, g, b, 1.0)
     bpy.context.scene.collection.children.link(streamline_collection)
 
-    return mat, streamline_collection
+    return mat, streamline_collection, n
 
 def arc_length(cart_points):
     lengths = [0.0]
@@ -173,22 +228,26 @@ class VisualizeStreamlines(Operator):
     bl_label = "Visualize Streamlines"
 
     def execute(self, context): 
-        mat, streamline_collection = mat_nodes(context)  
-        for i, streamline in enumerate(streamlines):
-            curve_data = bpy.data.hair_curves.new(f'streamline_{i}')
-            curve_data.add_curves([len(streamline)])
+        mat, streamline_collection, n = mat_nodes(context)
+        curve_data = bpy.data.hair_curves.new(f'run_{n}_curves')
+        curve_data.add_curves([len(s) for s in streamlines])
+        attr = curve_data.attributes.new('arc_param', 'FLOAT', 'POINT')
+        radius_attr = curve_data.attributes.new('radius', 'FLOAT', 'POINT')
+        phase_attr = curve_data.attributes.new('phase', 'FLOAT', 'CURVE')
+        point_idx = 0
+        for i, streamline in enumerate(streamlines): 
             cart_points = [convert_to_cart(p[0], p[1], p[2]) for p in streamline]
             stream_lens = arc_length(cart_points)
-            attr = curve_data.attributes.new('arc_param', 'FLOAT', 'POINT')
-            radius_attr = curve_data.attributes.new('radius', 'FLOAT', 'POINT')
+            phase_attr.data[i].value = random.random()
             for j, point in enumerate(cart_points):
                 x, y, z = point
-                curve_data.position_data[j].vector = (x, y, z)
-                attr.data[j].value = stream_lens[j] / stream_lens[-1] if stream_lens[-1] > 0 else 0.0  
-                radius_attr.data[j].value = 0.1
-            obj = bpy.data.objects.new(f'streamline{i}', curve_data)
-            obj.data.materials.append(mat)
-            streamline_collection.objects.link(obj)
+                curve_data.position_data[point_idx].vector = (x, y, z)
+                attr.data[point_idx].value = stream_lens[j] / stream_lens[-1] if stream_lens[-1] > 0 else 0.0  
+                radius_attr.data[point_idx].value = 0.1
+                point_idx += 1
+        obj = bpy.data.objects.new(f'run_{n}', curve_data)
+        obj.data.materials.append(mat)
+        streamline_collection.objects.link(obj)
         return {'FINISHED'}
 
 class TREE_PT_panel(bpy.types.Panel):
@@ -207,6 +266,25 @@ class TREE_PT_panel(bpy.types.Panel):
         layout.operator(ComputeStreamlines.bl_idname)
         layout.operator(VisualizeStreamlines.bl_idname)
 
+class TREE_PT_seeds(bpy.types.Panel):
+    bl_label = "Seeds"
+    bl_idname = "TREE_PT_seeds"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "TREE"
+    bl_parent_id = "TREE_PT_panel"
+        
+    def draw(self, context):
+        props = context.scene.tree_props
+        layout = self.layout
+        layout.prop(props, "seeds_per_level")
+        layout.prop(props, "alt_min")
+        layout.prop(props, "alt_max") 
+        layout.prop(props, "alt_step")
+        layout.prop(props, "anim_speed")
+        layout.prop(props, "spot_width")
+        layout.prop(props, "spot_strength")
+
 
 def register():
     bpy.utils.register_class(ImportData)
@@ -215,10 +293,12 @@ def register():
     bpy.utils.register_class(ComputeStreamlines)
     bpy.utils.register_class(VisualizeStreamlines)
     bpy.utils.register_class(TREE_PT_panel)
+    bpy.utils.register_class(TREE_PT_seeds)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 def unregister():
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import) 
+    bpy.utils.unregister_class(TREE_PT_seeds)
     bpy.utils.unregister_class(TREE_PT_panel)
     bpy.utils.unregister_class(VisualizeStreamlines)
     bpy.utils.unregister_class(ComputeStreamlines)  
@@ -231,4 +311,4 @@ if __name__ == "__main__":
     register()
     
     
-    
+# #76F3E41A - nice wind color
